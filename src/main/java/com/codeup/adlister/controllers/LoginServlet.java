6c99b9ac887247e4b127edb187552c20e0a02b2e
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-06-05 15:46:55 -0500

    feat:Finished working on LoginServlet

--- src/main/java/com/codeup/adlister/controllers/LoginServlet.java
+++ src/main/java/com/codeup/adlister/controllers/LoginServlet.java
@@ -33,8 +33,8 @@ public class LoginServlet extends HttpServlet {
         }
 //        BCrypt.checkpw(password, user.getPassword())
 //        boolean validAttempt =  BCrypt.checkpw(password, user.getPassword());
-        boolean validAttempt =  Password.check(password, user.getPassword());
-
+//        boolean validAttempt =  Password.check(password, user.getPassword());
+            boolean validAttempt = password.equals(user.getPassword());
         if (validAttempt) {
             request.getSession().setAttribute("user", user);
             response.sendRedirect("/profile");
--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -23,11 +23,12 @@ public class MySQLUsersDao implements Users {
 
 
     @Override
-    public User findByUsername(String username) {
+    public User findByUsername(String user_name) {
         String query = "SELECT * FROM users WHERE username = ? LIMIT 1";
+
         try {
             PreparedStatement stmt = connection.prepareStatement(query);
-            stmt.setString(1, username);
+            stmt.setString(1, user_name);
             return extractUser(stmt.executeQuery());
         } catch (SQLException e) {
             throw new RuntimeException("Error finding a user by username", e);
@@ -36,7 +37,7 @@ public class MySQLUsersDao implements Users {
 
     @Override
     public Long insert(User user) {
-        String query = "INSERT INTO users(username, email, password, firstName, lastName) VALUES (?, ?, ?,?,?)";
+        String query = "INSERT INTO users(username, email, password, first_name, last_name) VALUES (?, ?, ?,?,?)";
         try {
             PreparedStatement stmt = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
             stmt.setString(1, user.getUsername());
@@ -61,11 +62,11 @@ public class MySQLUsersDao implements Users {
         }
         return new User(
             rs.getLong("id"),
-            rs.getString("username"),
+            rs.getString("user_name"),
             rs.getString("email"),
             rs.getString("password"),
-                rs.getString("firstName"),
-                rs.getString("lastName")
+                rs.getString("first_name"),
+                rs.getString("last_name")
         );
     }
 
