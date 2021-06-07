441001cc29818f05bd1099fcea17ca1ec64e54d1
Salim Ahmedabadi <ahmedabadi.salim@gmail.com>
2021-06-06 19:42:44 -0500

    feat - Modified file

--- src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -24,17 +24,14 @@ public class MySQLUsersDao implements Users {
 
     @Override
     public User findByUsername(String username) {
-
-
-
-
+        String query = "SELECT * FROM users WHERE username = ? LIMIT 1";
         try {
-            String query = "SELECT * FROM users WHERE username = ? LIMIT 1";
+
             PreparedStatement stmt = connection.prepareStatement(query);
             stmt.setString(1, username);
             return extractUser(stmt.executeQuery());
         } catch (SQLException e) {
-            throw new RuntimeException("Error finding a user by username", e);
+            throw new RuntimeException("Error finding a user by user", e);
         }
     }
 
