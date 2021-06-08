3284525928a979720b2e3e944f3636b303aca9a0
Roland Valdez <valdez.roland.23@gmail.com>
2021-06-08 09:39:49 -0500

    merging johns code

--- src/main/java/com/codeup/adlister/controllers/HelloWorldServlet.java
+++ src/main/java/com/codeup/adlister/controllers/HelloWorldServlet.java
@@ -1,7 +1,5 @@
 package com.codeup.adlister.controllers;
-
 import com.codeup.adlister.dao.DaoFactory;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,14 +7,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
-
 @WebServlet(name = "controllers.HelloWorldServlet", urlPatterns = "/home")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-
-        request.setAttribute("ads", DaoFactory.getAdsDao().all());
-        request.getRequestDispatcher("/WEB-INF/index.jsp").forward(request, response);
-
-
+        request.setAttribute("ads", DaoFactory.getAdsDao().all() );
+        request.getRequestDispatcher("/index.jsp").forward(request, response);
     }
 }
