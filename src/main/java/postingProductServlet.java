6d7d79f92822aee683298995749d4ec29f74d505
John Pierce <pierce.johndaniel@gmail.com>
2021-06-08 09:13:02 -0500

    feat: update

--- src/main/java/postingProductServlet.java
+++ src/main/java/postingProductServlet.java
@@ -12,47 +12,47 @@ public class postingProductServlet extends HttpServlet {
 
     }
 }
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-//import javax.servlet.ServletException;
-//import javax.servlet.annotation.WebServlet;
-//import javax.servlet.http.HttpServlet;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//
-//@WebServlet(name="postingProduct", urlPatterns = "/postingProduct")
-//public class postingProductServlet extends HttpServlet {
-//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//
-//    }
-//
-//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
-//
-//            String category = request.getParameter("category");
-//            String description = request.getParameter("description");
-//            String condition = request.getParameter("condition");
-//
-//
-////       request.setAttribute("pickedColor", pickedColor);
-//
-//            request.setAttribute("newcolor", pickedColor);
-////        response.sendRedirect("/newPickedColor.jsp");
-//            request.getRequestDispatcher("/newPickedColor.jsp").forward(request, response);
-//
-//
-//        }
+@WebServlet(name="postingProduct", urlPatterns = "/postingProduct")
+public class postingProductServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
 
+            String category = request.getParameter("category");
+            String description = request.getParameter("description");
+            String condition = request.getParameter("condition");
 
 
- //  }
+//       request.setAttribute("pickedColor", pickedColor);
+
+            request.setAttribute("newcolor", pickedColor);
+//        response.sendRedirect("/newPickedColor.jsp");
+            request.getRequestDispatcher("/newPickedColor.jsp").forward(request, response);
 
 
-//
-//
-//
-//    }
-//}
+        }
+
+
+
+
+   }
+
+
+
+
+
+    }
+}
 
